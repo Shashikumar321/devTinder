fe0ee69011290581227e3b52e8cbd10dda5265c8
Shashikumar <shashikumar.n@carrier.com>
2025-03-08 19:09:17 +0530

    Added cors and corsConfiguration

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const { connectDB } = require("./config/database");
 const cookieParser = require("cookie-parser");
-
 const authRouter = require("./routes/authRouter");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+
+app.use(cors({
+  origin : "http://localhost:5173",
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
